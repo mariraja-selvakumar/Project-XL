0c0c205d2bb451e3980e2c9a5ff5c73211ecfe6b
Mariraja Selvakumar <marirajaselvakumar2000@gmail.com>
2024-04-27 15:36:21 +0530

    CORS Policy Added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const createError = require("http-errors");
+const cors = require("cors");
 const excelRoute = require("./route/file-upload.route");
 
 const app = express();
@@ -9,7 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use("/", excelRoute);
+app.use(cors());
+app.use("/api/", excelRoute);
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
